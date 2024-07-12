b29ff3da528650387252c72bbff51333a3364c1a
69kwan69 <69hoangphi69@gmail.com>
2024-07-12 14:28:49 +0700

    fixed missing context

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MyContext from '../contexts/MyContext';
 
 class Home extends Component {
   static contextType = MyContext; // using this.context to access global state
@@ -6,7 +7,9 @@ class Home extends Component {
   render() {
     return (
       <div className="p-10">
-        <h2 className="font-bold text-3xl">Ohayo, {this.context.username} nii-chan!</h2>
+        <h2 className="font-bold text-3xl">
+          Ohayo, {this.context.username} nii-chan!
+        </h2>
         <img
           className="block w-full"
           src="https://w7.pngwing.com/pngs/225/339/png-transparent-internet-meme-pol-anime-meme-child-mammal-hand.png"
