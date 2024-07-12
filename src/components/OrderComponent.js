aeec2daf65fdd01a6ba315262837cea79c233638
69kwan69 <69hoangphi69@gmail.com>
2024-07-12 15:28:17 +0700

    redesigned order page

--- src/components/OrderComponent.js
+++ src/components/OrderComponent.js
@@ -4,6 +4,7 @@ import MyContext from '../contexts/MyContext';
 
 class Order extends Component {
   static contextType = MyContext; // using this.context to access global state
+
   constructor(props) {
     super(props);
     this.state = {
@@ -11,52 +12,64 @@ class Order extends Component {
       order: null,
     };
   }
+
   render() {
-    const orders = this.state.orders.map((item) => {
+    const orders = this.state.orders.map((item, index) => {
       return (
         <tr
           key={item._id}
-          className="datatable"
+          className={
+            index % 2 === 0
+              ? 'hover:bg-slate-100'
+              : 'hover:bg-slate-100 bg-slate-50'
+          }
           onClick={() => this.trItemClick(item)}
         >
-          <td>{item._id}</td>
-          <td>{new Date(item.cdate).toLocaleString()}</td>
-          <td>{item.customer.name}</td>
-          <td>{item.customer.phone}</td>
-          <td>{item.total}</td>
-          <td>{item.status}</td>
-          <td>
-            {item.status === 'PENDING' ? (
+          <td className="p-4 text-left">{item._id}</td>
+          <td className="p-4 text-left">
+            {new Date(item.cdate).toLocaleString()}
+          </td>
+          <td className="p-4 text-left">{item.customer.name}</td>
+          <td className="p-4 text-left">{item.customer.phone}</td>
+          <td className="p-4 text-left">{item.total}</td>
+          <td className="p-4 text-left">{item.status}</td>
+          <td className="p-4 text-left">
+            {item.status === 'PENDING' && (
               <div>
-                <span
-                  className="link"
+                <button
+                  className="hover:bg-slate-200 normal-case border rounded px-2 py-1"
                   onClick={() => this.lnkApproveClick(item._id)}
                 >
-                  APPROVE
-                </span>{' '}
-                ||{' '}
-                <span
-                  className="link"
-                  onClick={() => this.lnkCancelClick(item._id)}
+                  Approve
+                </button>
+                <button
+                  className="hover:bg-red-600 hover:text-white normal-case border rounded px-2 py-1"
+                  onClick={(e) => this.lnkCancelClick(e, item._id)}
                 >
-                  CANCEL
-                </span>
+                  Cancel
+                </button>
               </div>
-            ) : (
-              <div />
             )}
           </td>
         </tr>
       );
     });
+
     if (this.state.order) {
       var items = this.state.order.items.map((item, index) => {
         return (
-          <tr key={item.product._id} className="datatable">
-            <td>{index + 1}</td>
-            <td>{item.product._id}</td>
-            <td>{item.product.name}</td>
-            <td>
+          <tr
+            className={
+              index % 2 === 0
+                ? 'hover:bg-slate-100'
+                : 'hover:bg-slate-100 bg-slate-50'
+            }
+            key={item.product._id}
+          >
+            <td className="p-4 text-left">{index + 1}</td>
+            <td className="p-4 text-left">{item.product._id}</td>
+            <td className="p-4 text-left">{item.product.name}</td>
+            <td className="p-4 text-left">
               <img
                 src={'data:image/jpg;base64,' + item.product.image}
                 width="70px"
@@ -64,52 +77,55 @@ class Order extends Component {
                 alt=""
               />
             </td>
-            <td>{item.product.price}</td>
-            <td>{item.quantity}</td>
-            <td>{item.product.price * item.quantity}</td>
+            <td className="p-4 text-left">{item.product.price}</td>
+            <td className="p-4 text-left">{item.quantity}</td>
+            <td className="p-4 text-left">
+              {item.product.price * item.quantity}
+            </td>
           </tr>
         );
       });
     }
+
     return (
-      <div>
-        <div className="align-center">
-          <h2 className="text-center">ORDER LIST</h2>
-          <table className="datatable" border="1">
-            <tbody>
-              <tr className="datatable">
-                <th>ID</th>
-                <th>Creation date</th>
-                <th>Cust.name</th>
-                <th>Cust.phone</th>
-                <th>Total</th>
-                <th>Status</th>
-                <th>Action</th>
+      <div className="p-10">
+        <div>
+          <h1 className="font-bold text-2xl uppercase mb-6">Orders</h1>
+
+          <table className="table-auto border-collapse border w-full">
+            <thead>
+              <tr className="border-b bg-slate-300">
+                <th className="p-4 text-left">ID</th>
+                <th className="p-4 text-left">Creation date</th>
+                <th className="p-4 text-left">Cust.name</th>
+                <th className="p-4 text-left">Cust.phone</th>
+                <th className="p-4 text-left">Total</th>
+                <th className="p-4 text-left">Status</th>
+                <th className="p-4 text-left">Action</th>
               </tr>
-              {orders}
-            </tbody>
+            </thead>
+            <tbody>{orders}</tbody>
           </table>
         </div>
-        {this.state.order ? (
-          <div className="align-center">
-            <h2 className="text-center">ORDER DETAIL</h2>
-            <table className="datatable" border="1">
-              <tbody>
-                <tr className="datatable">
-                  <th>No.</th>
-                  <th>Prod.ID</th>
-                  <th>Prod.name</th>
-                  <th>Image</th>
-                  <th>Price</th>
-                  <th>Quantity</th>
-                  <th>Amount</th>
+
+        {this.state.order && (
+          <div className="mt-10">
+            <h2 className="text-xl uppercase mb-6">Details</h2>
+            <table className="table-auto border-collapse border w-full">
+              <thead>
+                <tr className="border-b bg-slate-300">
+                  <th className="p-4 text-left">No.</th>
+                  <th className="p-4 text-left">Prod.ID</th>
+                  <th className="p-4 text-left">Prod.name</th>
+                  <th className="p-4 text-left">Image</th>
+                  <th className="p-4 text-left">Price</th>
+                  <th className="p-4 text-left">Quantity</th>
+                  <th className="p-4 text-left">Amount</th>
                 </tr>
-                {items}
-              </tbody>
+              </thead>
+              <tbody>{items}</tbody>
             </table>
           </div>
-        ) : (
-          <div />
         )}
       </div>
     );
